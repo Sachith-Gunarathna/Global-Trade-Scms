8af6992ec09e0d9e14727c0cbe7664ae8b4c8141
Sachith Gunarathna <106460379+Sachith-Gunarathna@users.noreply.github.com>
2026-08-23 15:02:24 +0530

    v1.0.4 - Update dependencies and setup database

---
+++ scms-ejb/src/main/java/com/nexcentauri/scms/entity/Vendor.java
@@ -0,0 +1,35 @@
+package com.nexcentauri.scms.entity;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "vendors")
+public class Vendor implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false , unique = true)
+    private String email;
+
+    @Column(name = "performance_score")
+    private Double performanceScore;
+
+    public Vendor(){}
+
+    public Long getId() {return id;}
+    public void setId(Long id) {this.id = id;}
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
+    public String getEmail() {return email;}
+    public void setEmail(String email) {this.email = email;}
+    public Double getPerformanceScore() {return performanceScore;}
+    public void setPerformanceScore(Double performanceScore) {this.performanceScore = performanceScore;}
+
+}
